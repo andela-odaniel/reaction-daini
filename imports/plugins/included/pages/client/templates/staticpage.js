7585928cbc7311e4809e7be3f2d6fe9e671747cd
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-11-15 17:33:43 +0100

    Discard the update event and use conditional to call the upate method when the form is submitted

--- imports/plugins/included/pages/client/templates/staticpage.js
+++ imports/plugins/included/pages/client/templates/staticpage.js
@@ -19,13 +19,27 @@ Template.staticPage.events({
         const title = target.title.value;
         const body = target.body.value;
 
-        console.log(name, title, body, Meteor.userId());
 
-        // Calls the freaking insert method with the form values as params
-        Meteor.call('pages.insert', name, title, body);
+        const btn = document.getElementById('btn-update').innerHTML;
+        
+        if(btn === 'Create Page'){
+
+            // Calls the freaking insert method with the form values as params
+            Meteor.call('pages.insert', name, title, body);
+            
+        }else{
+
+            //Call update method
+            Meteor.call('pages.update', currentPage, name, title, body);
+
+            // Reset button innerHTML
+            document.getElementById('btn-update').innerHTML = 'Create Page';
+        }
+            
 
         // Empties form after submission
-        target.name.value = ""; target.title.value = "";
+        target.name.value = ""; 
+        target.title.value = "";
         CKEDITOR.instances.body.setData('');
 
     },
@@ -45,29 +59,6 @@ Template.staticPage.events({
 
         currentPage = this._id;
 
-    },
-
-    // Update the page
-    'click .update'(event) {
-        event.preventDefault();
-
-        const name = document.getElementById('name').value;
-        const title = document.getElementById('title').value;
-        const body = CKEDITOR.instances.body.getData();
-
-
-        /**
-         * Call update method
-         */
-        Meteor.call('pages.update', currentPage, name, title, body);
-
-        // Empties form values after update
-        document.getElementById('name').value = ""; 
-        document.getElementById('title').value = "";
-        CKEDITOR.instances.body.setData('');
-
-        // Reset button innerHTML
-        document.getElementById('btn-update').innerHTML = 'Create Page';
     },
 
     // Oh buddy please! Delete the freaking page
